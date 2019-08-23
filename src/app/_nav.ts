33c52c4473e1c5b7cd664c0e7f9c985c63d90b6d
Pradyumna <f20180290@pilani.bits-pilani.ac.in>
2019-08-24 00:49:34 +0530

    Completely fixed the nav bar.

--- src/app/_nav.ts
+++ src/app/_nav.ts
@@ -34,14 +34,15 @@ export const navItems: NavData[] = [
     name: 'Dashboard',
     url: '/dashboard',
     icon: 'icon-speedometer',
-    badge: {
-      variant: 'info',
-      text: 'NEW'
-    }
   },
   {
     title: true,
     name: 'Add Research Objects'
+  },
+  {
+    name: "Upload RO's",
+    url: '/add/upload',
+    icon: 'icon-cloud-upload'
   },
   {
     name: 'Add from GitHub',
@@ -51,12 +52,8 @@ export const navItems: NavData[] = [
   {
     name: 'Add from FigShare',
     url: '/add/FigShare',
-    icon: 'icon-cloud-upload'
-  },
-  {
-    name: "Upload RO's",
-    url: '/add/upload',
-    icon: 'icon-cloud-upload'
+    icon: 'icon-cloud-upload',
+    attributes: { disabled: true }
   },
   {
     title: true,
@@ -67,213 +64,213 @@ export const navItems: NavData[] = [
     url: '/enrich/',
     icon: 'icon-docs'
   },
-  {
-    title: true,
-    name: 'Theme'
-  },
-  {
-    name: 'Colors',
-    url: '/theme/colors',
-    icon: 'icon-drop'
-  },
-  {
-    name: 'Typography',
-    url: '/theme/typography',
-    icon: 'icon-pencil'
-  },
-  {
-    title: true,
-    name: 'Components'
-  },
-  {
-    name: 'Base',
-    url: '/base',
-    icon: 'icon-puzzle',
-    children: [
-      {
-        name: 'Cards',
-        url: '/base/cards',
-        icon: 'icon-puzzle'
-      },
-      {
-        name: 'Carousels',
-        url: '/base/carousels',
-        icon: 'icon-puzzle'
-      },
-      {
-        name: 'GitHub',
-        url: '/base/github',
-        icon: 'icon-cursor'
-      },
-      {
-        name: 'Forms',
-        url: '/base/forms',
-        icon: 'icon-puzzle'
-      },
-      {
-        name: 'Pagination',
-        url: '/base/paginations',
-        icon: 'icon-puzzle'
-      },
-      {
-        name: 'Popovers',
-        url: '/base/popovers',
-        icon: 'icon-puzzle'
-      },
-      {
-        name: 'Progress',
-        url: '/base/progress',
-        icon: 'icon-puzzle'
-      },
-      {
-        name: 'Switches',
-        url: '/base/switches',
-        icon: 'icon-puzzle'
-      },
-      {
-        name: 'Tables',
-        url: '/base/tables',
-        icon: 'icon-puzzle'
-      },
-      {
-        name: 'Tabs',
-        url: '/base/tabs',
-        icon: 'icon-puzzle'
-      },
-      {
-        name: 'Tooltips',
-        url: '/base/tooltips',
-        icon: 'icon-puzzle'
-      }
-    ]
-  },
-  {
-    name: 'Buttons',
-    url: '/buttons',
-    icon: 'icon-cursor',
-    children: [
-      {
-        name: 'Buttons',
-        url: '/buttons/buttons',
-        icon: 'icon-cursor'
-      },
-      {
-        name: 'Dropdowns',
-        url: '/buttons/dropdowns',
-        icon: 'icon-cursor'
-      },
-      {
-        name: 'Brand Buttons',
-        url: '/buttons/brand-buttons',
-        icon: 'icon-cursor'
-      }
-    ]
-  },
-  {
-    name: 'Charts',
-    url: '/charts',
-    icon: 'icon-pie-chart'
-  },
-  {
-    name: 'Icons',
-    url: '/icons',
-    icon: 'icon-star',
-    children: [
-      {
-        name: 'CoreUI Icons',
-        url: '/icons/coreui-icons',
-        icon: 'icon-star',
-        badge: {
-          variant: 'success',
-          text: 'NEW'
-        }
-      },
-      {
-        name: 'Flags',
-        url: '/icons/flags',
-        icon: 'icon-star'
-      },
-      {
-        name: 'Font Awesome',
-        url: '/icons/font-awesome',
-        icon: 'icon-star',
-        badge: {
-          variant: 'secondary',
-          text: '4.7'
-        }
-      },
-      {
-        name: 'Simple Line Icons',
-        url: '/icons/simple-line-icons',
-        icon: 'icon-star'
-      }
-    ]
-  },
-  {
-    name: 'Notifications',
-    url: '/notifications',
-    icon: 'icon-bell',
-    children: [
-      {
-        name: 'Alerts',
-        url: '/notifications/alerts',
-        icon: 'icon-bell'
-      },
-      {
-        name: 'Badges',
-        url: '/notifications/badges',
-        icon: 'icon-bell'
-      },
-      {
-        name: 'Modals',
-        url: '/notifications/modals',
-        icon: 'icon-bell'
-      }
-    ]
-  },
-  {
-    name: 'Widgets',
-    url: '/widgets',
-    icon: 'icon-calculator',
-    badge: {
-      variant: 'info',
-      text: 'NEW'
-    }
-  },
-  {
-    divider: true
-  },
-  {
-    title: true,
-    name: 'Extras',
-  },
-  {
-    name: 'Pages',
-    url: '/pages',
-    icon: 'icon-star',
-    children: [
-      {
-        name: 'Login',
-        url: '/login',
-        icon: 'icon-star'
-      },
-      {
-        name: 'Register',
-        url: '/register',
-        icon: 'icon-star'
-      },
-      {
-        name: 'Error 404',
-        url: '/404',
-        icon: 'icon-star'
-      },
-      {
-        name: 'Error 500',
-        url: '/500',
-        icon: 'icon-star'
-      }
-    ]
-  },
+  // {
+  //   title: true,
+  //   name: 'Theme'
+  // },
+  // {
+  //   name: 'Colors',
+  //   url: '/theme/colors',
+  //   icon: 'icon-drop'
+  // },
+  // {
+  //   name: 'Typography',
+  //   url: '/theme/typography',
+  //   icon: 'icon-pencil'
+  // },
+  // {
+  //   title: true,
+  //   name: 'Components'
+  // },
+  // {
+  //   name: 'Base',
+  //   url: '/base',
+  //   icon: 'icon-puzzle',
+  //   children: [
+  //     {
+  //       name: 'Cards',
+  //       url: '/base/cards',
+  //       icon: 'icon-puzzle'
+  //     },
+  //     {
+  //       name: 'Carousels',
+  //       url: '/base/carousels',
+  //       icon: 'icon-puzzle'
+  //     },
+  //     {
+  //       name: 'GitHub',
+  //       url: '/base/github',
+  //       icon: 'icon-cursor'
+  //     },
+  //     {
+  //       name: 'Forms',
+  //       url: '/base/forms',
+  //       icon: 'icon-puzzle'
+  //     },
+  //     {
+  //       name: 'Pagination',
+  //       url: '/base/paginations',
+  //       icon: 'icon-puzzle'
+  //     },
+  //     {
+  //       name: 'Popovers',
+  //       url: '/base/popovers',
+  //       icon: 'icon-puzzle'
+  //     },
+  //     {
+  //       name: 'Progress',
+  //       url: '/base/progress',
+  //       icon: 'icon-puzzle'
+  //     },
+  //     {
+  //       name: 'Switches',
+  //       url: '/base/switches',
+  //       icon: 'icon-puzzle'
+  //     },
+  //     {
+  //       name: 'Tables',
+  //       url: '/base/tables',
+  //       icon: 'icon-puzzle'
+  //     },
+  //     {
+  //       name: 'Tabs',
+  //       url: '/base/tabs',
+  //       icon: 'icon-puzzle'
+  //     },
+  //     {
+  //       name: 'Tooltips',
+  //       url: '/base/tooltips',
+  //       icon: 'icon-puzzle'
+  //     }
+  //   ]
+  // },
+  // {
+  //   name: 'Buttons',
+  //   url: '/buttons',
+  //   icon: 'icon-cursor',
+  //   children: [
+  //     {
+  //       name: 'Buttons',
+  //       url: '/buttons/buttons',
+  //       icon: 'icon-cursor'
+  //     },
+  //     {
+  //       name: 'Dropdowns',
+  //       url: '/buttons/dropdowns',
+  //       icon: 'icon-cursor'
+  //     },
+  //     {
+  //       name: 'Brand Buttons',
+  //       url: '/buttons/brand-buttons',
+  //       icon: 'icon-cursor'
+  //     }
+  //   ]
+  // },
+  // {
+  //   name: 'Charts',
+  //   url: '/charts',
+  //   icon: 'icon-pie-chart'
+  // },
+  // {
+  //   name: 'Icons',
+  //   url: '/icons',
+  //   icon: 'icon-star',
+  //   children: [
+  //     {
+  //       name: 'CoreUI Icons',
+  //       url: '/icons/coreui-icons',
+  //       icon: 'icon-star',
+  //       badge: {
+  //         variant: 'success',
+  //         text: 'NEW'
+  //       }
+  //     },
+  //     {
+  //       name: 'Flags',
+  //       url: '/icons/flags',
+  //       icon: 'icon-star'
+  //     },
+  //     {
+  //       name: 'Font Awesome',
+  //       url: '/icons/font-awesome',
+  //       icon: 'icon-star',
+  //       badge: {
+  //         variant: 'secondary',
+  //         text: '4.7'
+  //       }
+  //     },
+  //     {
+  //       name: 'Simple Line Icons',
+  //       url: '/icons/simple-line-icons',
+  //       icon: 'icon-star'
+  //     }
+  //   ]
+  // },
+  // {
+  //   name: 'Notifications',
+  //   url: '/notifications',
+  //   icon: 'icon-bell',
+  //   children: [
+  //     {
+  //       name: 'Alerts',
+  //       url: '/notifications/alerts',
+  //       icon: 'icon-bell'
+  //     },
+  //     {
+  //       name: 'Badges',
+  //       url: '/notifications/badges',
+  //       icon: 'icon-bell'
+  //     },
+  //     {
+  //       name: 'Modals',
+  //       url: '/notifications/modals',
+  //       icon: 'icon-bell'
+  //     }
+  //   ]
+  // },
+  // {
+  //   name: 'Widgets',
+  //   url: '/widgets',
+  //   icon: 'icon-calculator',
+  //   badge: {
+  //     variant: 'info',
+  //     text: 'NEW'
+  //   }
+  // },
+  // {
+  //   divider: true
+  // },
+  // {
+  //   title: true,
+  //   name: 'Extras',
+  // },
+  // {
+  //   name: 'Pages',
+  //   url: '/pages',
+  //   icon: 'icon-star',
+  //   children: [
+  //     {
+  //       name: 'Login',
+  //       url: '/login',
+  //       icon: 'icon-star'
+  //     },
+  //     {
+  //       name: 'Register',
+  //       url: '/register',
+  //       icon: 'icon-star'
+  //     },
+  //     {
+  //       name: 'Error 404',
+  //       url: '/404',
+  //       icon: 'icon-star'
+  //     },
+  //     {
+  //       name: 'Error 500',
+  //       url: '/500',
+  //       icon: 'icon-star'
+  //     }
+  //   ]
+  // },
   // {
   //   name: 'Disabled',
   //   url: '/dashboard',
