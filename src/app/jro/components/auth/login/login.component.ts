586e6a717aa86fbbbb14f2330e66deb538c3c108
Pradyumna <f20180290@pilani.bits-pilani.ac.in>
2019-08-24 00:51:14 +0530

    Moved login component from to jro/auth/login

--- src/app/jro/components/auth/login/login.component.ts
+++ src/app/jro/components/auth/login/login.component.ts
@@ -1,30 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-import { environment } from '../../../environments/environment'
+import { environment } from '../../../../../environments/environment';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { Location } from '@angular/common';
-import { AuthService } from '../../jro/services/auth/auth.service';
-import { StorageService } from '../../jro/services/storage/storage.service'; 
-import { ResearcherService } from '../../jro/services/researcher/researcher.service';
+import { AuthService } from '../../../../jro/services/auth/auth.service';
+import { StorageService } from '../../../../jro/services/storage/storage.service'; 
+import { ResearcherService } from '../../../../jro/services/researcher/researcher.service';
+
 @Component({
   selector: 'app-dashboard',
-	templateUrl: 'login.component.html',
-	providers: [AuthService, StorageService, ResearcherService]
+  templateUrl: './login.component.html',
+  providers: [AuthService, StorageService, ResearcherService]
 
 })
 export class LoginComponent implements OnInit {
-
   user: Object;
 
   constructor(
    		public router: Router,
     	private activatedRoute: ActivatedRoute,
-		private location: Location,
-		private authService: AuthService,
-		private storageService: StorageService
+	  	private location: Location,
+	  	private authService: AuthService,
+	  	private storageService: StorageService
   ){ }
 
   ngOnInit() {
-    console.log("Hello");
     this.activatedRoute.queryParams.subscribe((params: Params) => {
 			let code = params['code'];
 			if (typeof code !== 'undefined' && localStorage.getItem('isLoggedin') === null) {
@@ -42,7 +41,6 @@ export class LoginComponent implements OnInit {
 		});
 
   }
-
 
   onLoggedin() {
 		var oauthWindow = window.open(`https://sandbox.orcid.org/oauth/authorize?client_id=${environment.orcidClientId}&response_type=code&scope=/authenticate&redirect_uri=${window.location.href}`, "_self", "toolbar=no, scrollbars=yes, width=500, height=600, top=500, left=500");
